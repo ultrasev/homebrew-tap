322a9c6bd3aaa1326bb08803ba2bdd8ed9300898
ultrasev <i@ultrasev.com>
2026-01-01 01:25:36 +0800

    fix(Casks/pulse.rb): correct quarantine attribute for Pulse app

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -15,6 +15,12 @@ cask "pulse" do
 
   app "Pulse.app"
 
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/Pulse.app"],
+                   sudo: false
+  end
+
   zap trash: [
     "~/Library/Application Support/com.pulse.dev",
     "~/Library/Caches/com.pulse.dev",
