b85731df01f8d4bb5c21498175f00902c2b85a1a
ultrasev <i@ultrasev.com>
2026-01-01 13:28:06 +0800

    fix(Casks/pulse.rb): 添加 v0.0.3 的 sha256 校验值

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -1,11 +1,11 @@
 cask "pulse" do
   version "0.0.3"
-  sha256 :no_check  # 首次发布后用实际 sha256 替换
 
-  # Apple Silicon
   if Hardware::CPU.arm?
+    sha256 "557b19788251d16e2ac0acd24504abbe22962a91a5aee39f8a45807451861b6c"
     url "https://github.com/ultrasev/pulse/releases/download/v#{version}/Pulse_#{version}_aarch64.dmg"
   else
+    sha256 "db8ecdfc08072e74aeb7c3e2d4e39fb8ad131436baeec99ba0b63a3c316afdf5"
     url "https://github.com/ultrasev/pulse/releases/download/v#{version}/Pulse_#{version}_x64.dmg"
   end
 
